581ac7cceca134bd3da385db9fdae9be33339d9e
= <=>
2023-03-30 15:33:52 -0300

    Adicionando plotagem.py

---
+++ lista1/plotagem.py
@@ -0,0 +1,22 @@
+# Arquivo destinado a aos códigos para plotagem das funções no R3
+
+# Código Exemplo da Matplotlib para superfícies 3D: https://matplotlib.org/stable/plot_types/3D/surface3d_simple.html
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Make data
+X = np.arange(-5, 5, 0.25)
+Y = np.arange(-5, 5, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X**2 + Y**2)
+Z = np.sin(R)
+
+# Plot the surface
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+ax.plot_surface(X, Y, Z, vmin=Z.min() * 2)
+
+ax.set(xticklabels=[],
+       yticklabels=[],
+       zticklabels=[])
+
+plt.show()
