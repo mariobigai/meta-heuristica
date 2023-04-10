dddecd597008157f1d5600f7abc620ab98b46adf
= <=>
2023-04-09 23:30:37 -0300

    Modificando plotagem do best_ponto

--- lista1/plotagem.py
+++ lista1/plotagem.py
@@ -3,13 +3,14 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
+import seaborn as sns
 
-def plota_3D(X, Y, Z, best_ponto=[0,0,0]):
+def plota_3D(X, Y, Z, best_ponto=None):
        #Cria figura e eixos
        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
        # Plota a superfície
-       surf = ax.plot_surface(X, Y, Z, cmap=cm.jet,
+       ax.plot_surface(X, Y, Z, cmap=cm.jet,
                               linewidth=0, antialiased=False)
 
        # Customização do eixo z.
@@ -19,6 +20,19 @@ def plota_3D(X, Y, Z, best_ponto=[0,0,0]):
        # A StrMethodFormatter is used automatically
        ax.zaxis.set_major_formatter('{x:.02f}')
 
-       ax.scatter(best_ponto[0], best_ponto[1], best_ponto[2], edgecolor='black', c='black' , marker='x')
+       if best_ponto is not None:
+              ax.scatter(best_ponto[0], best_ponto[1], best_ponto[2], c='black', marker='o', s=40)
 
+       plt.show()
+
+def plota_boxplot(dados, modelo_nome, nome_func):
+       #Coleta os dados para o boxplot
+       sns.boxplot(data= dados)
+       #Insere um titulo
+       plt.title(f'{modelo_nome} {nome_func}')
+       #Insere o nome para o eixo y
+       plt.ylabel("Gerações")
+       #Salva o boxplot
+       plt.savefig(f'./Boxplot{modelo_nome}{nome_func}')
+       #Plota o boxplot
        plt.show()
