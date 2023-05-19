f0e7015fc541cd0dcb73b6d01080762c302aec50
Guilherme Portela Meller <guilhermeportelameller@gmail.com>
2023-05-19 02:08:37 -0300

    Verificar inercia e coefs, alterado algumas lógicas

--- lista3/GuilhermeMario/bpso.py
+++ lista3/GuilhermeMario/bpso.py
@@ -1,91 +1,90 @@
 import random
-import time
+import numpy as np
 
-class Particle:
-    def __init__(self, num_dimensions):
-        self.position = [random.randint(0, 1) for _ in range(num_dimensions)]
-        self.velocity = [0] * num_dimensions
-        self.best_position = self.position.copy()
+# Função de avaliação (fitness)
+def evaluate_particle(particle):
+    return sum(particle)
 
-    def update_velocity(self, global_best_position, inertia_weight, cognitive_weight, social_weight):
-        for i in range(len(self.velocity)):
-            cognitive_component = cognitive_weight * random.random() * (self.best_position[i] - self.position[i])
-            social_component = social_weight * random.random() * (global_best_position[i] - self.position[i])
-            self.velocity[i] = inertia_weight * self.velocity[i] + cognitive_component + social_component
+# Função de inicialização da população
+def initialize_population(pop_size, num_dimensions):
+    population = []
+    for _ in range(pop_size):
+        particle = [random.randint(0, 1) for _ in range(num_dimensions)]
+        population.append(particle)
+    return population
 
-    def update_position(self):
-        for i in range(len(self.position)):
-            if random.random() < sigmoid(self.velocity[i]):
-                self.position[i] = 1
-            else:
-                self.position[i] = 0
+# Atualiza a melhor posição global
+def update_global_best(population, global_best, global_best_fitness):
+    for particle in population:
+        fitness = evaluate_particle(particle)
+        if fitness > global_best_fitness:
+            global_best = particle
+            global_best_fitness = fitness
+    return global_best, global_best_fitness
 
-    def evaluate_fitness(self):
-        return sum(self.position)
+# Atualiza a velocidade e posição da partícula
+def update_particle(particle, velocity, personal_best, global_best, inertia_weight, cognitive_weight, social_weight):
+    new_velocity = []
+    new_particle = []
+    for i in range(len(particle)):
+        inertia_term = inertia_weight * velocity[i]
+        cognitive_term = cognitive_weight * random.uniform(0, 1) * (personal_best[i] - particle[i])
+        social_term = social_weight * random.uniform(0, 1) * (global_best[i] - particle[i])
+        new_velocity.append(inertia_term + cognitive_term + social_term)
+        new_particle.append(1 if random.uniform(0, 1) < sigmoid(new_velocity[i]) else 0)
+    return new_particle, new_velocity
 
+# Função sigmoid para atualizar a posição binária da partícula
 def sigmoid(x):
-    return 1 / (1 + pow(2.71828, -x))
+    return 1 / (1 + np.exp(-x))
 
-def calculate_normalized_fitness(fitness, max_fitness):
-    return fitness / max_fitness
+# Algoritmo PSO binário
+def binary_pso(pop_size, num_dimensions):
+    inertia_weight = 1
+    cognitive_weight = 2
+    social_weight = 2
 
-def one_max_pso(num_particles, num_dimensions):
-    particles = [Particle(num_dimensions) for _ in range(num_particles)]
-    global_best_fitness = float('-inf')
-    global_best_position = particles[0].best_position.copy()
+    population = initialize_population(pop_size, num_dimensions)
+    velocity = [[random.uniform(-1, 1) for _ in range(num_dimensions)] for _ in range(pop_size)]
+    personal_best = population.copy()
+    personal_best_fitness = [evaluate_particle(particle) for particle in population]
 
-    fitness_values = []
-    start_time = time.time()
+    global_best_fitness = max(personal_best_fitness)
+    global_best_index = personal_best_fitness.index(global_best_fitness)
+    global_best = personal_best[global_best_index]
 
     iteration = 0
-    while global_best_fitness < num_particles:
-        iteration_best_fitness = float('-inf')
-        iteration_total_fitness = 0
+    while global_best_fitness < num_dimensions:
+        for i in range(pop_size):
+            particle, velocity[i] = update_particle(
+                population[i], velocity[i], personal_best[i], global_best, inertia_weight, cognitive_weight, social_weight
+            )
+            population[i] = particle
 
-        for particle in particles:
-            fitness = particle.evaluate_fitness()
-            if fitness > global_best_fitness:
-                particle.best_position = particle.position.copy()
-                global_best_fitness = fitness
-                global_best_position = particle.position.copy()
+            fitness = evaluate_particle(particle)
+            if fitness > personal_best_fitness[i]:
+                personal_best[i] = particle
+                personal_best_fitness[i] = fitness
 
-            iteration_best_fitness = max(iteration_best_fitness, fitness)
-            iteration_total_fitness += fitness
-
-        fitness_values.append(iteration_best_fitness)
-
-        for particle in particles:
-            particle.update_velocity(global_best_position, 1, 2, 2)  # Aumentando a velocidade
-            particle.update_position()
+        global_best, global_best_fitness = update_global_best(population, global_best, global_best_fitness)
 
         iteration += 1
         print(global_best_fitness)
 
-    end_time = time.time()
+    return global_best, personal_best_fitness, global_best_fitness, iteration
 
-    best_fitness = max(fitness_values)
-    average_fitness = sum(fitness_values) / len(fitness_values)
-    worst_fitness = min(fitness_values)
+# Parâmetros do PSO
+population_size = 50
+dimensions = 500
 
-    normalized_best_fitness = calculate_normalized_fitness(best_fitness, num_dimensions)
-    normalized_average_fitness = calculate_normalized_fitness(average_fitness, num_dimensions)
-    normalized_worst_fitness = calculate_normalized_fitness(worst_fitness, num_dimensions)
+# Executa o PSO binário para resolver o problema OneMax
+best_particle, particle_fitness, best_fitness, itera = binary_pso(population_size, dimensions)
 
-    execution_time = end_time - start_time
-
-    return global_best_position, best_fitness, average_fitness, worst_fitness, normalized_best_fitness, normalized_average_fitness, normalized_worst_fitness, execution_time, iteration
-
-# Exemplo de uso
-num_particles = 100
-num_dimensions = 100
-
-solution, best_fitness, average_fitness, worst_fitness, normalized_best_fitness, normalized_average_fitness, normalized_worst_fitness, execution_time, itera = one_max_pso(num_particles, num_dimensions)
-print("Melhor solução encontrada:", solution)
+# Imprime os resultados
 print("Número de iterações:", itera)
 print("Melhor fitness:", best_fitness)
-print("Média fitness:", average_fitness)
-print("Pior fitness:", worst_fitness)
-print("Melhor fitness temporal normalizado:", normalized_best_fitness)
-print("Média fitness temporal normalizado:", normalized_average_fitness)
-print("Pior fitness temporal normalizado:", normalized_worst_fitness)
-print("Tempo de execução:", execution_time, "segundos")
+print("Média do fitness:", np.mean(particle_fitness))
+print("Pior fitness:", min(particle_fitness))
+print("Melhor fitness temporal normalizado:", best_fitness / dimensions)
+print("Média do fitness temporal normalizado:", np.mean(particle_fitness) / dimensions)
+print("Pior fitness temporal normalizado:", min(particle_fitness) / dimensions)
